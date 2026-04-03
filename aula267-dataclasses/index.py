c816875593cfc299bcd0957019ccb3fbe130517f
Tiago Cabral <tiagocabral.gja@gmail.com>
2026-04-03 00:42:49 -0300

    começando estudo de dataclasses

---
+++ aula267-dataclasses/index.py
@@ -0,0 +1,20 @@
+# O que são dataclasses
+# O módulo dataclasses fornece um decorador e funções para criar métodos como 
+# __init__(), __repr__(), __eq__() (entre outros) em classes definidas pelo usuario
+# Em resumo dataclasses são syntax sugar para criar classes normais
+from dataclasses import dataclass # <- decorator
+
+@dataclass
+class Pessoa:
+    nome: str
+    sobrenome: str
+    
+    @property
+    def nome_completo(self):
+        return f'{self.nome} {self.sobrenome}'
+    
+
+if __name__ == "__main__":
+    p1 = Pessoa('Tiago', 'Cabral')
+    print(p1)
+    print(p1.nome_completo)
